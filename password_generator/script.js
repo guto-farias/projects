d4be567c48482b15775e43401540111c678e8091
Vítor Augusto Farias <gutofariasdev@gmail.com>
2024-03-04 22:56:06 -0300

    adds 04/03/2024

--- password_generator/script.js
+++ password_generator/script.js
@@ -1,9 +1,32 @@
+let passwordLength = 20
+let finalPassword = ""
+
 function generatePassword(){
-    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789?!@&*,.()[]"
+    let chars = "abcdefghijklmnopqrstuvwxyz"
+
+    const upperCaseEl = document.querySelector("#uppercase-check")
+    const numberEl = document.querySelector("#number-check")
+    const symbolEl = document.querySelector("#symbol-check")
+
+    const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const numberChars = "0123456789"
+    const symbolChars = "?!@&*,.()[]"
+
+    if(upperCaseEl.checked){
+        chars += upperCaseChars
+    }
+
+    if(numberEl.checked){
+        chars += numberChars
+    }
+
+    if(symbolEl.checked){
+        chars += symbolChars
+    }
 
     let password = ""
 
-    for(let i = 0; i < 8; i++){
+    for(let i = 0; i < passwordLength; i++){
         const randomNumber = Math.floor(Math.random() * chars.length)
 
         password += chars.substring(randomNumber, randomNumber + 1) //recorte de uma string
@@ -11,7 +34,19 @@ function generatePassword(){
 
     const inputEl = document.getElementById("password")
     inputEl.value = password
-    console.log(password)
+    finalPassword = inputEl.value
 
 }
 
+function catchRange(){ //pego valor do input range e chamo generatePassword a cada alteração 
+    const passwordLengthEl = document.querySelector("#password-length")
+    passwordLengthEl.addEventListener("input", function(){
+        passwordLength = passwordLengthEl.value
+        document.querySelector("#password-length-text").innerText = passwordLength
+        generatePassword()
+    })
+}
+
+function copy(){
+    navigator.clipboard.writeText(finalPassword)//copiar conteúdo do inputEl, API padrão atualmente
+}
